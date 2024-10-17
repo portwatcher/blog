4cedf8fb23b7f6455896185a7793966e656d593f
PortWatcher <jury@skyquakers.com>
2024-10-17 15:58:30 +0900

    feat: cache neodb response to reduce it's overhead

--- server/api/shelves/index.get.ts
+++ server/api/shelves/index.get.ts
@@ -2,6 +2,14 @@ const isShelfType = (type: string): type is ShelfType => {
   return ['wishlist', 'progress', 'complete'].includes(type)
 }
 
+const caches = new Map<
+  string,
+  {
+    shelf: Shelf
+    updatedAt: number
+  }
+>()
+
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
   const page = Number(query.page) || 1
@@ -22,11 +30,23 @@ export default defineEventHandler(async (event) => {
   const url = new URL(`https://neodb.social/api/me/shelf/${query.type}`)
   url.searchParams.append('page', String(page))
 
+  const cache = caches.get(url.toString())
+  if (cache && cache.updatedAt > Date.now() - 1000 * 60 * 60 * 24) {
+    return cache.shelf
+  }
+
   const res = await fetch(url.toString(), {
     headers: {
-      Authorization: `Bearer ${config.neodbKey}`
-    }
+      Authorization: `Bearer ${config.neodbKey}`,
+    },
   })
 
-  return await res.json()
+  const data = await res.json()
+
+  caches.set(url.toString(), {
+    shelf: data,
+    updatedAt: Date.now(),
+  })
+
+  return data
 })
